df85f2a1cda35cc5554cff24ca1473bf703ac5ae
vinay <vinaykumarbeelagi@gmail.com>
2022-12-16 14:34:49 +0530

    running-sum-of-1d-array

--- array/richest-customer-wealth.js
+++ array/richest-customer-wealth.js
@@ -35,3 +35,17 @@ var numberOfSteps = function(num) {
 };
 console.log(numberOfSteps(num = 14)); 
 //output: 6
+
+//https://leetcode.com/problems/running-sum-of-1d-array
+var runningSum = function(nums) {
+    let sum = [];
+    let partialSum = 0;
+    for(let i=0;i<nums.length;i++){        
+        partialSum = partialSum + nums[i]
+        sum.push(partialSum);
+        
+    }
+    return sum;
+}
+console.log(runningSum ([1,2,3,4]));
+//output: [1, 3, 6, 10]
