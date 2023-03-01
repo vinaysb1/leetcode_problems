b35119921a2a3b3c7b0f56d5755cfe12868e4873
vinay <vinaykumarbeelagi@gmail.com>
2023-03-01 10:53:32 +0530

    average-salary-excluding-the-minimum-and-maximum-salary

---
+++ programming skills/avg salary.js
@@ -0,0 +1,16 @@
+// average-salary-excluding-the-minimum-and-maximum-salary/
+var average = function(salary) {
+    var length=salary.length
+    var sum=0;
+    minValue = Math.min(...salary);
+    maxValue = Math.max(...salary);
+    for(i=0;i<length;i++){
+        if(salary[i]!=minValue ){
+            if(salary[i]!=maxValue){
+        sum=sum+salary[i];
+            }
+        }
+    }
+    return sum/(length-2)
+    
+};
