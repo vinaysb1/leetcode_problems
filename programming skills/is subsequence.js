9dd284bdeef0cf930cc4dd3c4c63b7426a319b3b
vinay <vinaykumarbeelagi@gmail.com>
2023-03-23 23:09:59 +0530

    is subsequence

---
+++ programming skills/is subsequence.js
@@ -0,0 +1,15 @@
+// is-subsequence-solutions
+
+
+const isSubsequence = (s, t, index = 0) => {
+
+    if (s.length > t.length) return false;
+
+    for (let i = 0; i < t.length; i++) {
+        if (s[index] == t[i]) {
+            index++;
+            if (index == s.length) break;
+        }
+    }
+    return s.length === index;
+};
