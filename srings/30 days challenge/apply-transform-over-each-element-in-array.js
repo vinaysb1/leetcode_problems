ae1203ecbd396734d060830cfa9f5c64add19b92
vinay <vinaykumarbeelagi@gmail.com>
2023-09-07 13:01:09 +0530

    apply transform over each element in array

---
+++ srings/30 days challenge/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,9 @@
+// apply-transform-over-each-element-in-array
+
+var map = function(arr, fn) {
+    let newArr =[];
+    for(let i=0;i<arr.length;i++){
+        newArr.push(fn(arr[i],i));
+    }
+    return newArr;
+};
